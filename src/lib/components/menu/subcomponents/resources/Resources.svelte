240053f159d324e9bc358637a626e892929f8eef
GuessImNick <stewartnick53@icloud.com>
2024-08-22 09:04:09 -0500

    scroll and zoom fixes

--- src/lib/components/menu/subcomponents/resources/Resources.svelte
+++ src/lib/components/menu/subcomponents/resources/Resources.svelte
@@ -6,7 +6,7 @@
 </script>
 
 {#if $appState.isMenuOpen}
-	<div class="w-full max-h-[calc(100vh-80px)] overflow-scroll scrollbar-hidden scroll-smooth">
+	<div class="w-full max-h-[calc(100vh-80px)] overflow-scroll scrollbar-hidden scroll-smooth ios-scroll">
 		<div class="flex flex-col">
 			<div
 				class="w-full text-center flex px-4 flex-col justify-between items-center my-4 py-2 sticky top-[-1px] bg-black h-full"
